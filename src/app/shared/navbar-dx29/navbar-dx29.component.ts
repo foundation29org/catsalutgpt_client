81fd020f7f84f5571d3428554de130c829e004f0
Javier <javierlogronoaguirre@gmail.com>
2024-08-21 13:44:58 +0200

    added langs

--- src/app/shared/navbar-dx29/navbar-dx29.component.ts
+++ src/app/shared/navbar-dx29/navbar-dx29.component.ts
@@ -2,6 +2,7 @@ import { Component, HostListener, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
+import { LangService } from 'app/shared/services/lang.service';
 import { EventsService } from 'app/shared/services/events.service';
 import { Injector } from '@angular/core';
 
@@ -10,10 +11,13 @@ declare let gtag: any;
 @Component({
   selector: 'app-navbar-dx29',
   templateUrl: './navbar-dx29.component.html',
-  styleUrls: ['./navbar-dx29.component.scss']
+  styleUrls: ['./navbar-dx29.component.scss'],
+  providers: [LangService]
 })
 
 export class NavbarD29Component implements OnDestroy {
+  currentLang = 'en';
+  langs: any;
   toggleClass = 'ft-maximize';
   placement = "bottom-right";
   public isCollapsed = true;
@@ -25,7 +29,8 @@ export class NavbarD29Component implements OnDestroy {
   private subscription: Subscription = new Subscription();
   isMenuExpanded = false;
 
-  constructor(public translate: TranslateService, private router: Router, private inj: Injector) {
+  constructor(public translate: TranslateService, private langService: LangService, private router: Router, private inj: Injector) {
+    this.loadLanguages();
     this.router.events.filter((event: any) => event instanceof NavigationEnd).subscribe(
 
       event => {
@@ -54,6 +59,51 @@ export class NavbarD29Component implements OnDestroy {
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  loadLanguages() {
+    this.subscription.add(this.langService.getLangs()
+      .subscribe((res: any) => {
+        this.langs = res;
+        if (sessionStorage.getItem('lang')) {
+          this.translate.use(sessionStorage.getItem('lang'));
+          this.searchLangName(sessionStorage.getItem('lang'));
+        } else {
+          const browserLang: string = this.translate.getBrowserLang();
+          var foundlang = false;
+          for (let lang of this.langs) {
+            if (browserLang.match(lang.code)) {
+              this.translate.use(lang.code);
+              foundlang = true;
+              sessionStorage.setItem('lang', lang.code);
+              this.searchLangName(lang.name);
+            }
+          }
+          if (!foundlang) {
+            sessionStorage.setItem('lang', this.translate.store.currentLang);
+          }
+        }
+
+      }, (err) => {
+        console.log(err);
+      }));
+  }
+
+  searchLangName(code: string) {
+    for (let lang of this.langs) {
+      var actualLang = sessionStorage.getItem('lang');
+      if (actualLang == lang.code) {
+        this.currentLang = lang.code;
+      }
+    }
+  }
+
+  ChangeLanguage(language: string) {
+    this.translate.use(language);
+    sessionStorage.setItem('lang', language);
+    this.searchLangName(language);
+    var eventsLang = this.inj.get(EventsService);
+    eventsLang.broadcast('changelang', language);
   }
 
   ToggleClass() {
