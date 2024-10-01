a10c784e7fb92d182679ea426198da80e70e86f0
Javier <javierlogronoaguirre@gmail.com>
2024-10-01 11:46:16 +0200

    change proyect name

--- src/app/pages/land/land-page-routing.module.ts
+++ src/app/pages/land/land-page-routing.module.ts
@@ -13,11 +13,11 @@ const routes: Routes = [
         path: '.',
         component: UndiagnosedPageComponent,
         data: {
-          title: 'CatSalutGPT'
+          title: 'IASalutAjudaDx'
         },
       },
       {
-        path: 'sobrecatsalutgpt',
+        path: 'sobreiasalutajudadx',
         component: AboutUsPageComponent,
         data: {
           title: 'menu.About us'
